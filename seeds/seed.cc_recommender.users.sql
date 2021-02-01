6b028bafe1409ef24bc154523d1e76536cae12ea
Ryan <leeyuenhigh@gmail.com>
2021-02-01 09:27:07 -0500

    Created seed file for users

---
+++ seeds/seed.cc_recommender.users.sql
@@ -0,0 +1,7 @@
+INSERT INTO users(email,hashedPassword,userCards,msg)
+VALUES
+  ('hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C',[1,2],''),
+  ('2hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$5kOG/hdDJJB8xRNDaT9jbuSYfKc3rWtIFj.zdCLF45Z1E61MGvsYC',[1,2,3],''),
+  ('3hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$52IFOba30w8yQUEF3wfqPOy3hq31ujasIr0cQu6RFcD0GURuZE4wi',[1],''),
+  ('4hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$52IFOba30w8yQUEF3wfqPOy3hq31ujasIr0cQu6RFcD0GURuZE4wi',[],'')
+;
