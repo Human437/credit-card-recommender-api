7b0262dddc8d7912964fa23a3f3a54d71c36b0a0
Ryan <leeyuenhigh@gmail.com>
2021-02-02 16:43:33 -0500

    Created fixtures to generate dummy data for users, articles, available_cards

---
+++ test/cc_recommender-fixtures.js
@@ -0,0 +1,141 @@
+function makeUsersArray(){
+  return [
+    {
+      "id":1,
+      "email":"hwpyxoutfugfqbusvz@twzhhq.com",
+      "hashedpassword":"$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C",
+      "usercards":[1,2],
+      // Only use unhashedPassword for testing purposes
+      // unhashedPassword: aB1!bnmv
+      "msg":"",
+    },
+    {
+      "id":2,
+      "email":"2hwpyxoutfugfqbusvz@twzhhq.com",
+      "hashedpassword":"$2a$10$5kOG/hdDJJB8xRNDaT9jbuSYfKc3rWtIFj.zdCLF45Z1E61MGvsYC",
+      "usercards":[1,2,3],
+      // Only use unhashedPassword for testing purposes
+      // unhashedPassword: aB2!bnmv
+      "msg":"",
+    },
+    {
+      "id":3,
+      "email":"3hwpyxoutfugfqbusvz@twzhhq.com",
+      "hashedpassword":"$2a$10$52IFOba30w8yQUEF3wfqPOy3hq31ujasIr0cQu6RFcD0GURuZE4wi",
+      "usercards":[1],
+      // Only use unhashedPassword for testing purposes
+      // unhashedPassword: aB3!bnmv
+      "msg":"",
+    },
+    {
+      "id":4,
+      "email":"4hwpyxoutfugfqbusvz@twzhhq.com",
+      "hashedpassword":"$2a$10$52IFOba30w8yQUEF3wfqPOy3hq31ujasIr0cQu6RFcD0GURuZE4wi",
+      "usercards":[],
+      // Only use unhashedPassword for testing purposes
+      // unhashedPassword: aB3!bnmv
+      "msg":"",
+    }
+  ]
+}
+
+function makeAvailableCardsArray(){
+  return [
+    {
+      "id":1,
+      "title": "Discover IT Student",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo.",
+      "image": "Place holder"
+    },
+    {
+      "id": 2,
+      "title": "Chase Freedom",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo.",
+      "image": "Place holder"
+    },
+    {
+      "id": 3,
+      "title": "Chase Sapphire Prefered",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo.",
+      "image": "Place holder"
+    },    
+    {
+      "id": 4,
+      "title": "Chase Sapphire Reserved",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo.",
+      "image": "Place holder"
+    },    
+    {
+      "id": 5,
+      "title": "Discover IT Cash Back Credit Card",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo.",
+      "image": "Place holder"
+    },
+    {
+      "id": 6,
+      "title": "American Express Gold Card",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo.",
+      "image": "Place holder"
+    },
+    {
+      "id": 7,
+      "title": "American Express Platinum Card",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo.",
+      "image": "Place holder"
+    },
+    {
+      "id": 8,
+      "title": "Chase Freedom Unlimited",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo.",
+      "image": "Place holder"
+    },
+    {
+      "id": 9,
+      "title": "Discover It Secured",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo.",
+      "image": "Place holder"
+    },
+    {
+      "id": 10,
+      "title": "Chase Freedom Flex",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo.",
+      "image": "Place holder"
+    } 
+  ]
+}
+
+function makeArticlesArray(){
+  return [
+    {
+      "id": 1,
+      "title": "Choosing Your First Credit Card",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo."
+    },
+    {
+      "id": 2,
+      "title": "Your First Five Cards",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo."
+    },
+    {
+      "id": 3,
+      "title": "Understanding Your Fico Score",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo."
+    },
+    {
+      "id": 4,
+      "title": "Unspoken Issuer Rules",
+      "contennt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo."
+    },
+    {
+      "id": 5,
+      "title": "Cashback vs Points",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui mattis, bibendum dui aliquet, lobortis ante. Cras vestibulum, quam ac tincidunt dapibus, sapien mauris hendrerit mauris, ut scelerisque nisl magna sed ipsum. Sed molestie tortor vitae dignissim volutpat. In tincidunt leo imperdiet elit pulvinar, in rhoncus nulla pretium. Duis id malesuada risus. Phasellus libero nunc, feugiat at pharetra sit amet, vulputate ut justo."
+    }
+  ]
+}
+
+module.exports = {
+  makeArticlesArray,
+  makeAvailableCardsArray,
+  makeUsersArray
+}
