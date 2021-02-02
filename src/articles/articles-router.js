e1472188bfd5e05d5623b27c6a41d7c90e71b031
Ryan <leeyuenhigh@gmail.com>
2021-02-02 00:59:20 -0500

    Added route to get article by id

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -23,4 +23,26 @@ articlesRouter
       .catch(error => next(error))
   })
 
+articlesRouter
+  .route('/:articleId')
+  .all((req, res, next) => {
+    ArticlesService.getArticleById(
+      req.app.get('db'),
+      req.params.articleId
+    )
+      .then(article => {
+        if (!article) {
+          return res.status(404).json({
+            error: { message: `Article doesn't exist` }
+          })
+        }
+        res.article = article
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeArticle(res.article))
+  })
+
 module.exports = articlesRouter
