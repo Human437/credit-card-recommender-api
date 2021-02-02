f3dec09f774f6fa6d9797c123a45390d1c0e7bd7
Ryan <leeyuenhigh@gmail.com>
2021-02-02 17:03:18 -0500

    Added unauthorized request test for users endpoints

--- test/cc_recommender.spec.js
+++ test/cc_recommender.spec.js
@@ -1,5 +1,6 @@
 const app = require('../src/app')
 const knex = require('knex')
+const fixtures = require('./cc_recommender-fixtures')
 
 describe('Credit Card Recommender Endpoints', () => {
   let db
@@ -12,18 +13,57 @@ describe('Credit Card Recommender Endpoints', () => {
     app.set('db', db)
   })
 
-  // after('disconnect from db', () => db.destroy())
+  after('disconnect from db', () => db.destroy())
 
-  // before('cleanup', () => db('cc_recommender_test').truncate())
+  before('cleanup', () => db('users').truncate())
 
-  // afterEach('cleanup', () => db('cc_recommender_test').truncate())
+  afterEach('cleanup', () => db('users').truncate())
 
   describe(`Unauthorized requests`, () => {
-    it('responds with 401 Unauthorized for GET /', () => {
+    const testUsers = fixtures.makeUsersArray()
+    const testCards = fixtures.makeAvailableCardsArray()
+    const testArticles = fixtures.makeArticlesArray()
+
+    beforeEach('insert Users', () => {
+      return db
+        .into('users')
+        .insert(testUsers)
+    })
+
+    it('responds with 401 Unauthorized for GET /api/users', () => {
       return supertest(app)
-        .get('/')
+        .get('/api/users')
         .expect(401, { error: 'Unauthorized request' })
     })
 
+    it('responds with 401 Unauthorized for GET /api/users/:userId', () => {
+      const secondUser = testUsers[1]
+      return supertest(app)
+        .get(`/api/users/${secondUser.id}`)
+        .expect(401, { error: 'Unauthorized request' })
+    })
+
+    it('responds with 401 Unauthorized for POST /api/users', () => {
+      return supertest(app)
+        .post('/api/users')
+        .send({
+          email:'testemail@gmail.com',
+          hashedPassowrd: 'dfafdafdkasklfdakljfdakl',
+          usercards: '[1,2,3]',
+          msg: ''
+        })
+        .expect(401, { error: 'Unauthorized request' })
+    })
+
+    it('responds with 401 Unauthorized for POST /api/users', () => {
+      return supertest(app)
+        .patch('/api/users')
+        .send({
+          id:1,
+          usercards: '[3]',
+          msg: 'Test msg'
+        })
+        .expect(401, { error: 'Unauthorized request' })
+    })
   })
 })
