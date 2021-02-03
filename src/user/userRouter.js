a2fa010f9ee4e1a89c3887939a49471fe7787bdc
Ryan <leeyuenhigh@gmail.com>
2021-02-02 23:03:14 -0500

    Resolved issue created from adding 404 error when user is not found

--- src/user/userRouter.js
+++ src/user/userRouter.js
@@ -51,23 +51,37 @@ userRouter
 
   .patch(jsonParser,(req,res,next) => {
     const knexInstance = req.app.get('db')
-    const {id, userCards:usercards,msg} = req.body
+    const {id, usercards:usercards,msg} = req.body
     const userInfoToUpdate = {usercards,msg}
 
-    const numberOfValues = Object.values(userInfoToUpdate).filter(Boolean).length
-    if (numberOfValues === 0) {
+    if (typeof usercards === 'undefined' && typeof msg === 'undefined') {
       return res.status(400).json({
         error: {
           message: `Request body must contain either 'userCards' or 'msg'`
         }
       })
     }
-
-    UserService.updateUser(knexInstance,id,userInfoToUpdate)
-    .then(numRowsAffected => {
-      res.status(204).end()
-    })
-    .catch(next)
+    if (typeof id === 'undefined'){
+      return res.status(400).json({
+        error: {
+          message: 'Request body must contain id'
+        }
+      })
+    }
+    UserService.getUserById(knexInstance,id)
+      .then(user => {
+        if(!user){
+          return res.status(404).json({
+            error: {message: `User doesn't exist`}
+          })
+        }
+        return UserService.updateUser(knexInstance,id,userInfoToUpdate)
+          .then(numRowsAffected => {
+            res.status(204).end()
+          })
+          .catch(next)
+      })
+      .catch(next)
   })
 
 userRouter
